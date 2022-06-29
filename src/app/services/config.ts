638a7d7b146f207962bd032ce6ae8a4848a2fa55
Brian Thiely <brian.thiely@gmail.com>
2022-06-29 14:32:59 +0200

    change path apiUrl heroku

--- src/app/services/config.ts
+++ src/app/services/config.ts
@@ -1,4 +1,4 @@
 export let apiUrl =
 	location.hostname === 'localhost' || location.hostname === '127.0.0.1'
 		? 'http://localhost:3000'
-		: 'https://hot-takes-oc.herokuapp.com/';
+		: 'https://hot-takes-oc.herokuapp.com';
