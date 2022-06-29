6b85f3355a7ddde7129cd683eabe61e2991dfe2c
Brian Thiely <brian.thiely@gmail.com>
2022-06-29 14:09:32 +0200

    refct config.ts

--- src/app/services/config.ts
+++ src/app/services/config.ts
@@ -1,3 +1,4 @@
-let apiUrl = (location.hostname === 'localhost' || location.hostname ==='127.0.0.1') 
-? 'http://localhost:3000' 
-: 'https://hot-takes-oc.herokuapp.com/';
+let apiUrl =
+	location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+		? 'http://localhost:3000'
+		: 'https://hot-takes-oc.herokuapp.com/';
